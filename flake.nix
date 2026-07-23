b15d8d6d2c76fd81683ace3a533ade55d77440da
Rob David <rob.j.david@googlemail.com>
2026-07-23 12:21:14 +0000

    Add Nix flake and .envrc files

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development shell with Helm";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              gopls
+              delve
+              kubernetes-helm
+            ];
+            hardeningDisable = [ "fortify" ];
+          };
+        }
+      );
+    };
+}
